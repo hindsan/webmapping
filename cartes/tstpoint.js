63c65155a5fc8b71c09c9316bdea4a337a023823
hamza ben <hamzaben@Hamzas-MBP.home>
2019-01-27 16:20:24 +0100

    Initial Commit

---
+++ cartes/tstpoint.js
@@ -0,0 +1,48 @@
+
+var Villes =
+{
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::4261" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Tanger" }, "geometry": { "type": "Point", "coordinates": [ -5.909252254607655, 35.781249505366233 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Tétouan" }, "geometry": { "type": "Point", "coordinates": [ -5.383936563420114, 35.590750408561966 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Asilah" }, "geometry": { "type": "Point", "coordinates": [ -6.03047895257401, 35.486841810305087 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Larache" }, "geometry": { "type": "Point", "coordinates": [ -6.157478350443525, 35.18666141534078 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Chefchaouen" }, "geometry": { "type": "Point", "coordinates": [ -5.297346064872718, 35.18666141534078 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Al Hoceima" }, "geometry": { "type": "Point", "coordinates": [ -3.94076158763017, 35.261706514081858 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "SNador" }, "geometry": { "type": "Point", "coordinates": [ -2.942084504383528, 35.180888715437618 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Saadia" }, "geometry": { "type": "Point", "coordinates": [ -2.243587816101194, 35.088525516987062 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Oujda" }, "geometry": { "type": "Point", "coordinates": [ -1.897225821911608, 34.690209223669036 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Taza" }, "geometry": { "type": "Point", "coordinates": [ -4.015806686371247, 34.228393231416256 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Ouezzane" }, "geometry": { "type": "Point", "coordinates": [ -5.568662960321228, 34.811435921635393 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Kénitra" }, "geometry": { "type": "Point", "coordinates": [ -6.567340043567869, 34.268802130738372 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Salé" }, "geometry": { "type": "Point", "coordinates": [ -6.80979343950058, 34.060984934224621 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Rabat" }, "geometry": { "type": "Point", "coordinates": [ -6.855975038725858, 34.009030635096181 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Sidi Kacem" }, "geometry": { "type": "Point", "coordinates": [ -5.724525857706541, 34.228393231416256 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Fès" }, "geometry": { "type": "Point", "coordinates": [ -5.014483769617889, 34.060984934224621 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Meknès" }, "geometry": { "type": "Point", "coordinates": [ -5.580208360127547, 33.899349336936147 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Khemisset" }, "geometry": { "type": "Point", "coordinates": [ -6.099751351411928, 33.83007693809823 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Ifrane" }, "geometry": { "type": "Point", "coordinates": [ -5.124165067777924, 33.529896543133923 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Khenifra" }, "geometry": { "type": "Point", "coordinates": [ -5.672571558578103, 32.958399252721101 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Oued Zem" }, "geometry": { "type": "Point", "coordinates": [ -6.567340043567869, 32.877581454076868 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Khouribga" }, "geometry": { "type": "Point", "coordinates": [ -6.936792837370096, 32.900672253689507 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Midelt" }, "geometry": { "type": "Point", "coordinates": [ -4.72007607455674, 32.710173156885233 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Errachidia" }, "geometry": { "type": "Point", "coordinates": [ -4.408350279786112, 31.959722169474464 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Beni Mellal" }, "geometry": { "type": "Point", "coordinates": [ -6.359522847054118, 32.363811162695647 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Settat" }, "geometry": { "type": "Point", "coordinates": [ -7.623744125846108, 33.010353551849541 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Berrechid" }, "geometry": { "type": "Point", "coordinates": [ -7.58910792642715, 33.26435234758857 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Casablanca" }, "geometry": { "type": "Point", "coordinates": [ -7.606426026136629, 33.59916894197184 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Mohammedia" }, "geometry": { "type": "Point", "coordinates": [ -7.427472329138675, 33.720395639938197 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "El Jadida" }, "geometry": { "type": "Point", "coordinates": [ -8.518512610835874, 33.26435234758857 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Safi" }, "geometry": { "type": "Point", "coordinates": [ -9.245872798634006, 32.271447964245091 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Essaouira" }, "geometry": { "type": "Point", "coordinates": [ -9.800051989337344, 31.445951878093243 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Agadir" }, "geometry": { "type": "Point", "coordinates": [ -9.60955289253307, 30.41725675535017 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Laayoun" }, "geometry": { "type": "Point", "coordinates": [ -13.100881793964103, 27.208790149173964 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Dakhla" }, "geometry": { "type": "Point", "coordinates": [ -15.93989560633808, 23.690906828188403 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Lagouira" }, "geometry": { "type": "Point", "coordinates": [ -17.082890187163716, 20.926938114555504 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Boujdour" }, "geometry": { "type": "Point", "coordinates": [ -14.391710696908591, 26.229776106257468 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nom": "Marrakech" }, "geometry": { "type": "Point", "coordinates": [ -7.987244116547622, 31.637764041323976 ] } }
+]
+}
+;
